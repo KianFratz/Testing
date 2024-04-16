1f3383de6e88a09346ab4148fd71fd60a10c00f1
meowwhat <132213728+KianFratz@users.noreply.github.com>
2024-04-16 21:02:09 +0800

    Add project files.

---
+++ Testing/Connection/Conn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace Testing.Connection
+{
+    internal class Conn
+    {
+        private string ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"C:\\Kian Fratz Pagobo\\Testing\\Testing\\bin\\Debug\\TestingDB.accdb\"";
+        private OleDbConnection connection;
+        public void OpenConnection()
+        {
+            connection = new OleDbConnection(ConnectionString); 
+            connection.Open();
+        }
+    }
+}
